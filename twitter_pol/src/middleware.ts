ae96834510d466fd0c57e9ca26e0ac6f5155b046
igormomcilovic <igormomc@hotmail.com>
2023-03-28 21:55:56 +0200

    project

---
+++ twitter_pol/src/middleware.ts
@@ -0,0 +1,12 @@
+import { withClerkMiddleware} from "@clerk/nextjs/server";
+import { NextResponse} from "next/server";
+import type { NextRequest } from "next/server";
+
+export default withClerkMiddleware(() =>{
+    return NextResponse.next()
+})
+
+// Stop Middleware from running on static files
+export const config = {
+    matcher: "/((?!_next/image|_next/static|favicon.ico).)*",
+};
---
+++ twitter_pol/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import {ClerkProvider} from "@clerk/nextjs";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
