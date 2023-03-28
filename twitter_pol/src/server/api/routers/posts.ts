e1af84923bed5abe47d3a573fcfefc61613ab002
igormomcilovic <igormomc@hotmail.com>
2023-03-29 00:30:58 +0200

    a lot of stuff

--- twitter_pol/src/server/api/routers/posts.ts
+++ twitter_pol/src/server/api/routers/posts.ts
@@ -1,14 +1,49 @@
-import { z } from "zod";
+import {z} from "zod";
 
 import {
     createTRPCRouter,
     publicProcedure,
     protectedProcedure,
 } from "~/server/api/trpc";
+import {clerkClient} from "@clerk/nextjs/server";
+import type {User} from "@clerk/nextjs/dist/api";
+import {TRPCError} from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+    return {
+        id: user.id,
+        username: user.username,
+        profileImageUrl: user.profileImageUrl,
+    };
+};
 
 export const postsRouter = createTRPCRouter({
+    getAll: publicProcedure.query(async ({ctx}) => {
+        const posts = await ctx.prisma.post.findMany({
+            take: 100,
+        });
 
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.post.findMany();
+        const users = (
+            await clerkClient.users.getUserList({
+                userId: posts.map((post) => post.autherId),
+                limit: 100,
+            })
+        ).map(filterUserForClient);
+
+        return posts.map((post) => {
+            const author = users.find((user) => user.id === post.autherId);
+            if (!author)
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Author for post not found",
+                });
+            return {
+                post,
+                author: {
+                    ...author,
+                    username: author.username,
+                }
+            };
+        });
     }),
 });
