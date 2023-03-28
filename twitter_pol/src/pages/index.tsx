bb0844a49035052bd0a325aa9bc6d1ed5dc3f69c
igormomcilovic <igormomc@hotmail.com>
2023-03-29 00:41:03 +0200

    next image

--- twitter_pol/src/pages/index.tsx
+++ twitter_pol/src/pages/index.tsx
@@ -8,6 +8,7 @@ import {SignIn, SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
 import {RouterOptions} from "express";
 import dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime'
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 
@@ -17,10 +18,12 @@ const CreatePostWizard = () => {
     console.log("data", user);
     return (
         <div className="flex w-full gap-3">
-            <img
+            <Image
                 src={user.profileImageUrl}
                 alt="profile image"
                 className="h-14 w-14 rounded-full"
+                width={56}
+                height={56}
             />
             <input
                 placeholder="Type something"
@@ -35,10 +38,12 @@ const PostView = (props: PostWithUser) => {
     const {post, author} = props;
     return (
         <div key={post.id} className="flex gap-3 p-4 border-b border-slate-400">
-            <img
+            <Image
                 src={author.profileImageUrl}
                 alt="profile image"
                 className="h-14 w-14 rounded-full"
+                width={56}
+                height={56}
             />
             <div className="flex flex-col">
                 <div className="flex gap-1 text-slate-300">
