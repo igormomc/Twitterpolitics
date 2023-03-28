d766f94d4ec1764c65da260826d7b73d94c2a870
igormomcilovic <igormomc@hotmail.com>
2023-03-28 22:25:42 +0200

    idk

--- twitter_pol/src/pages/index.tsx
+++ twitter_pol/src/pages/index.tsx
@@ -7,9 +7,9 @@ import { api } from "~/utils/api";
 import {SignIn, SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -21,6 +21,11 @@ const Home: NextPage = () => {
           <div>
               {!user.isSignedIn && <SignInButton />}
               {!!user.isSignedIn && <SignOutButton />}
+          </div>
+          <div>
+              {data?.map((post) => (
+                  <div key={post.id}>{post.content}</div>
+              ))}
           </div>
       </main>
     </>
